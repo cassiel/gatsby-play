b90838a9b6a93f265bb5f3e17e207da16d40ce8f
Nick (Git client) <nick@cassiel.com>
2020-12-23 12:08:24 +0000

    Intermediate check-in: styling etc.

---
+++ gatsby-site/src/pages/about-css-modules.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./about-css-modules.module.css";
+import Container from "../components/container";
+
+const About = () => (
+        <div className={styles.user}>Hello World</div>
+);
+
+console.log(styles);
+
+export default About;
